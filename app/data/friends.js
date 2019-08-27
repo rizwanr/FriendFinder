c2c483fba5dee4b920bcc466cc2779c385bd7319
Rizwan <rizwanrensa@gmail.com>
2019-08-27 00:59:47 -0400

    total difference and console logging the most compatible friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -30,6 +30,26 @@ let getFriendsData = [{
       1,
       5
     ]
+  },
+
+  {
+
+    "name": "Suvro",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      1,
+      3,
+      1,
+      2,
+      3,
+      5,
+      3,
+      2,
+      1,
+      2
+
+    ]
+
   }
 
 ]
